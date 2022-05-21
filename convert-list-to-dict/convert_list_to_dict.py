06ba65a8b79a45dbb1858c5c88afc829f659c442
gabrielbarragan <gabrielbarragan07@gmail.com>
2022-05-21 15:25:54 -0500

    listas nesteadas a diccionarios, terminado

--- convert-list-to-dict/convert_list_to_dict.py
+++ convert-list-to-dict/convert_list_to_dict.py
@@ -1,2 +1,8 @@
-"""code goes here"""
+def convert_list_to_dict(nested_list: list= []) -> dict:
+    """get a nested list and return a dict
+    input-> nested_list: list
+    output-> dict"""
 
+    my_dict = {key[0]:key[1] for key in nested_list }
+    
+    return my_dict
